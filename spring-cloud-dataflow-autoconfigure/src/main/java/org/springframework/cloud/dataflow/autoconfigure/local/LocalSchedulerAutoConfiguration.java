bf56fd236393c9fca8d08d6229b0556f050ef43e
Luca Cirillo <luca.cirillo@sisal.it>
2021-10-22 10:34:21 +0200

    first commit

--- spring-cloud-dataflow-autoconfigure/src/main/java/org/springframework/cloud/dataflow/autoconfigure/local/LocalSchedulerAutoConfiguration.java
+++ spring-cloud-dataflow-autoconfigure/src/main/java/org/springframework/cloud/dataflow/autoconfigure/local/LocalSchedulerAutoConfiguration.java
@@ -15,6 +15,11 @@
  */
 package org.springframework.cloud.dataflow.autoconfigure.local;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -32,6 +37,7 @@ import org.springframework.cloud.deployer.spi.scheduler.Scheduler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -54,23 +60,25 @@ public class LocalSchedulerAutoConfiguration {
             }
 
             public void unschedule(String scheduleName) {
-                throw new UnsupportedOperationException("Implementing...");
+                RestTemplate restTemplate = new RestTemplate();
+                ResponseEntity<String> executeResponse = restTemplate.postForEntity("http://dataflow-scheduler:8080/api/deleteJob?jobName="+scheduleName+"&jobGroup=sisal&jobClass=batch-job", null, String.class);
             }
 
             public List<ScheduleInfo> list(String taskDefinitionName) {
-                return Collections.emptyList();
+                return list();
             }
 
             public List<ScheduleInfo> list() {
 
                 RestTemplate restTemplate = new RestTemplate();
-                ResponseEntity<List<SchedulerJobInfo>> executeResponse = restTemplate.getForEntity("http://dataflow-scheduler:8080/api/getAllJobs", null, String.class);
+                ResponseEntity<SchedulerJobInfo[]> executeResponse = restTemplate.getForEntity("http://dataflow-scheduler:8080/api/getAllJobs", SchedulerJobInfo[].class);
 
                 if(executeResponse != null && executeResponse.getBody() != null) {
-                    return executeResponse.getBody().stream().map(s -> {
+                    return java.util.Arrays.asList(executeResponse.getBody()).stream().map(s -> {
                         ScheduleInfo scheduleInfo = new ScheduleInfo();
                         scheduleInfo.setScheduleName(s.getJobName());
                         scheduleInfo.setTaskDefinitionName(s.getJobName());
+                        scheduleInfo.setScheduleProperties(new java.util.HashMap<>());
                         return scheduleInfo;
                     }).collect(Collectors.toList());
                 }else{
@@ -81,21 +89,21 @@ public class LocalSchedulerAutoConfiguration {
     }
 
     @Bean
-    public SchedulerService schedulerService(){
+    public SchedulerService schedulerService(Resource resource){
         Scheduler scheduler = localScheduler();
         return new SchedulerService() {
 
             @Override
             public void schedule(String scheduleName, String taskDefinitionName, Map<String, String> taskProperties, List<String> commandLineArgs, String platformName) {
                 AppDefinition appDefinition = new AppDefinition(platformName, null);
-                ScheduleRequest scheduleRequest = new ScheduleRequest(appDefinition, taskProperties, null, scheduleName, null);
+                ScheduleRequest scheduleRequest = new ScheduleRequest(appDefinition, taskProperties, new java.util.HashMap<>(), scheduleName, resource);
                 scheduler.schedule(scheduleRequest);
             }
 
             @Override
             public void schedule(String scheduleName, String taskDefinitionName, Map<String, String> taskProperties, List<String> commandLineArgs) {
                 AppDefinition appDefinition = new AppDefinition("DAFAULT", null);
-                ScheduleRequest scheduleRequest = new ScheduleRequest(appDefinition, taskProperties, null, scheduleName, null);
+                ScheduleRequest scheduleRequest = new ScheduleRequest(appDefinition, taskProperties, new java.util.HashMap<>(), scheduleName, resource);
                 scheduler.schedule(scheduleRequest);
             }
 
@@ -156,6 +164,61 @@ public class LocalSchedulerAutoConfiguration {
 
             @Override
             public ScheduleInfo getSchedule(String scheduleName) {
+                return null;
+            }
+        };
+    }
+
+    @Bean
+    public Resource resource(){
+        return new Resource() {
+            @Override
+            public boolean exists() {
+                return true;
+            }
+
+            @Override
+            public URL getURL() throws IOException {
+                return null;
+            }
+
+            @Override
+            public URI getURI() throws IOException {
+                return null;
+            }
+
+            @Override
+            public File getFile() throws IOException {
+                return null;
+            }
+
+            @Override
+            public long contentLength() throws IOException {
+                return 0;
+            }
+
+            @Override
+            public long lastModified() throws IOException {
+                return 0;
+            }
+
+            @Override
+            public Resource createRelative(String s) throws IOException {
+                return null;
+            }
+
+            @Override
+            public String getFilename() {
+                return null;
+            }
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+
+            @Override
+            public InputStream getInputStream() throws IOException {
                 return null;
             }
         };
